4e98cee2e2aed99b3244fa5671e63c5e4a6a5c1b
uvini917 <65717498+uvini917@users.noreply.github.com>
2023-05-06 01:55:58 +0530

    scg functions

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import LogoSvg from 'assets/icons/logo';
+import {LogoHome} from 'assets/icons/logo';
 
 const Logo: React.FC = () => {
   return (
     <a className="block text-teal-600 dark:text-teal-600" href="/">
       <span className="sr-only">Home</span>
-      <LogoSvg />
+      <LogoHome />
     </a>
   );
 };
